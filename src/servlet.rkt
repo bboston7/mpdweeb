0089dd6a93a3a17abf0781bb4cd20e0174931a02
Brett Boston <bboston7@gmail.com>
2015-01-28 23:55:16 -0800

    Fixed timeout issues!

--- src/servlet.rkt
+++ src/servlet.rkt
@@ -1,6 +1,11 @@
 #lang web-server
 
 (require web-server/servlet-env
+         web-server/managers/timeouts
+         web-server/servlet/setup
+         web-server/servlet-dispatch
+         (prefix-in timeout: web-server/dispatchers/dispatch-timeout)
+         (prefix-in seq: web-server/dispatchers/dispatch-sequencer)
          "html-renderer.rkt")
 
 (provide interface-version stuffer start)
@@ -12,12 +17,20 @@
   (let ([path (map path/param-path (url-path (request-uri req)))])
     (displayln path)
     (path->response path)))
-    ;`(html (head (title "YOOOOOOOOOOOO"))
-    ;       (body (p "welcome to my lil server")))))
 
+(serve/launch/wait
+  (lambda (sema)
+    (seq:make (timeout:make 3600)
+              (dispatch/servlet start #:regexp #rx"")))
+  #:listen-ip #f
+  #:port 8080)
+#|
 (serve/servlet start
                #:command-line? #t
                #:listen-ip #f
                #:servlet-path "/"
                #:servlet-regexp #rx""
-               #:stateless? #t)
+               #:stateless? #f
+               #:port 8080
+               #:manager (create-timeout-manager false 3600 3600))
+|#
